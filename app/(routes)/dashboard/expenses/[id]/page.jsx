8f95345ad1bf9e73528c632b7f8c5ec88300cac3
Sudheer Singh <singhsudheer2005@gmail.com>
2025-06-10 14:27:24 +0530

    Display And Manage Expenses

--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -3,14 +3,16 @@
 import { db } from '@/utils/dbConfig';
 import { Budgets, Expenses } from '@/utils/schema';
 import { useUser } from '@clerk/nextjs';
-import { eq, getTableColumns, sql } from 'drizzle-orm';
+import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
 import React, { useEffect, use, useState } from 'react'; // ✅ Import `use` from React
 import BudgetItem from '../../budgets/_components/BudgetItem';
 import AddExpense from '../_components/AddExpense';
+import ExpenseListTable from '../_components/ExpenseListTable';
 
 function ExpensesScreen({ params }) {
     const { user, isLoaded } = useUser();
     const [budgetInfo, setBudgetInfo] = useState();
+    const [expensesList,setExpensesList] = useState([]);
 
     // ✅ unwrap the promise
     const resolvedParams = use(params); 
@@ -22,6 +24,9 @@ function ExpensesScreen({ params }) {
         }
     }, [isLoaded, user]);
 
+    /**
+     * Get Budget Information
+     */
     const getBudgetInformation = async () => {
         const result = await db
             .select({
@@ -36,7 +41,19 @@ function ExpensesScreen({ params }) {
             .groupBy(Budgets.id);
 
         setBudgetInfo(result[0]);
+        getExpensesList();
     };
+
+    /**
+     * Get Latest Expenses
+     */
+    const getExpensesList = async()=>{
+        const result = await db.select().from(Expenses)
+        .where(eq(Expenses.budgetId,budgetId))
+        .orderBy(desc(Expenses.id));
+        setExpensesList(result);
+        // console.log(result);
+    }
 
     return (
         <div className="p-10">
@@ -51,6 +68,14 @@ function ExpensesScreen({ params }) {
                     budgetId={budgetId} // ✅ fixed: using resolvedParams.id instead of params.id
                     user={user}
                      refreshData={()=>getBudgetInformation()}
+                />
+            </div>
+            <div className='mt-4'>
+                <h2 className='font-bold text-lg'>
+                    Latest Expenses
+                </h2>
+                <ExpenseListTable expensesList={expensesList}
+                refreshData={()=>getBudgetInformation()}
                 />
             </div>
         </div>
